82f4fd8de4990158c448fd4c26b896884dd16c49
ssaikia3 <ssaikia3@gmail.com>
2017-11-15 16:39:41 +0530

    3rd commit

---
+++ gmap/src/main/java/com/sstudio/gmap/Gmap.java
@@ -0,0 +1,61 @@
+package com.sstudio.gmap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class Gmap {
+    public Gmap() {
+    }
+    public String getDistance(final double lat1, final double lon1, final double lat2, final double lon2) {
+        final String[] parsedDistance = new String[1];
+        final String[] response = new String[1];
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    URL url = new URL("http://maps.googleapis.com/maps/api/directions/json?origin="
+                            + lat1 + "," + lon1 + "&destination=" + lat2 + "," + lon2
+                            + "&sensor=false&units=metric&mode=driving");
+                    final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    InputStream in = new BufferedInputStream(conn.getInputStream());
+                    response[0] = org.apache.commons.io.IOUtils.toString(in, "UTF-8");
+
+                    JSONObject jsonObject = new JSONObject(response[0]);
+                    JSONArray array = jsonObject.getJSONArray("routes");
+                    JSONObject routes = array.getJSONObject(0);
+                    JSONArray legs = routes.getJSONArray("legs");
+                    JSONObject steps = legs.getJSONObject(0);
+                    JSONObject distance = steps.getJSONObject("distance");
+                    parsedDistance[0] = distance.getString("text");
+
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return parsedDistance[0];
+    }
+}
